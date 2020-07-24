40f0913cfeb9fc67d7894bd786f28997c49ea703
Dr <yansinan@163.com>
2020-07-24 22:55:05 +0800

    run.sh bug /home/node

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 docker stop node
 docker rm node
-docker run -it --name=node -p 8797:8797 -v $HOME/ownAlbum:/hone/node --restart=always node fish
+docker run -it --name=node -p 8797:8797 -v $HOME/node:/home/node --restart=always node fish
