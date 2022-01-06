2936c56964df59aaab2933bc906e1d8dd0e36a97
Dr <yansinan@163.com>
2022-01-06 16:52:05 +0800

    文件夹映射app/public/upload -> /home/node/public/upload

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ docker stop dora
 docker rm dora
 echo $HOME
 docker run -it  \
--p 8084:8080 -p 8900:8900 -p 8899:8899 -p 8888:8888 -p 9229:9229 -p 9999:9999 -p 5800:5800 -p 9230:9230 \
+-p 80:8080 -p 443:8080 -p 8080:8080 -p 8900:8900 -p 8899:8899 -p 8888:8888 -p 9229:9229 -p 9999:9999 -p 5800:5800 -p 9230:9230 \
 --name=dora --network=nginxNet \
--v $HOME/node:/home/node --restart=always node:dora fish
+-v /home/node/workspace/nodejsWebsite:/home/node -v /home/node/public/upload:/home/node/app/public/upload  --restart=always node:dora fish
 
