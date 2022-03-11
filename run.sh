0ad9d13b2184488dcdfc3f93417ffd5879f5cce4
Dr <yansinan@163.com>
2022-03-11 23:12:40 +0800

    改为使用Nginx

--- run.sh
+++ run.sh
@@ -3,7 +3,8 @@ docker stop dora
 docker rm dora
 echo $HOME
 docker run -it  \
--p 80:8080 -p 443:8080 -p 8080:8080 -p 8900:8900 -p 8899:8899 -p 8888:8888 -p 9229:9229 -p 9999:9999 -p 5800:5800 -p 9230:9230 \
+# -p 80:8080 -p 443:8080 -p 8080:8080 -p 8900:8900 -p 8899:8899 -p 8888:8888 -p 9229:9229 -p 9999:9999 -p 5800:5800 -p 9230:9230 \
 --name=dora --network=nginxNet \
 -v /home/node/workspace/nodejsWebsite:/home/node -v /home/node/public/upload:/home/node/app/public/upload  --restart=always node:dora fish
 
+# docker run -it --name=rubyeye --network=nginxNet -v /home/node/workspace/nodejsWebsite:/home/node -v /home/node/public/upload:/home/node/app/public/upload  --restart=always dora:rubyeye fish
