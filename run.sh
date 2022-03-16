3be77c35f03d9dd4486ade23504dcf9adaa5ca5e
Dr <yansinan@163.com>
2022-03-16 17:23:08 +0800

    优化格式

--- run.sh
+++ run.sh
@@ -4,7 +4,12 @@ docker stop $nameContainer
 docker rm $nameContainer
 echo $(pwd)
 docker run -it  \
-# -p 8084:8080 -p 8900:8900 -p 8899:8899 -p 8888:8888 -p 9229:9229 -p 9999:9999 -p 5800:5800 -p 9230:9230 \
---name=$nameContainer --network=nginxNet \
--v $(pwd)/../..:/home/node --restart=always node:base fish
+--name=$nameContainer \
+--network=nginxNet \
+--rm \
+-v $(pwd)/../..:/home/node \
+node:base fish
 
+# --rm \
+#--restart=always \
+# -p 8084:8080 -p 8900:8900 -p 8899:8899 -p 8888:8888 -p 9229:9229 -p 9999:9999 -p 5800:5800 -p 9230:9230 \
